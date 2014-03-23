13d97d2bd3ba5f25da1f7291b8f867dc4f3e7dd7
Kamal Farsaoui <elfa.kamal@gmail.com>
2014-03-23 17:43:12 +0100

    better version of the amd-component directive

--- public/js/directives/amd-component.js
+++ public/js/directives/amd-component.js
@@ -1,6 +1,6 @@
 'use strict';
 
-define(["require", "jquery", "constants", "underscore"], function(require, $, constants, _) {
+define(["require", "lodash"], function(require, _) {
 
   var AMDComponent = function($compile, $rootScope) {
     return {
@@ -8,7 +8,7 @@ define(["require", "jquery", "constants", "underscore"], function(require, $, co
 
       link: function($scope, $element, $attrs)
       {
-        var component = $attrs[constants.AMD_DIRECTIVE_NAME];
+        var component = $attrs["amdComponent"];
 
         if(typeof component === "undefined" || typeof component !== "string" || component === null)
           throw new Error("component is undefined / not a string / null");
@@ -18,8 +18,8 @@ define(["require", "jquery", "constants", "underscore"], function(require, $, co
          * and its base template from the views.
          */
         var bootstrapComponent = function() {
-          var main = "../components/" + component + "/main";
-          var template = "text!../components/" + component + "/views/base.html";
+          var main = "../../components/" + component + "/main";
+          var template = "text!../../components/" + component + "/views/base.html";
 
           //load the component and its view
           require([main, template], function(Component, baseTemplate)
@@ -28,19 +28,12 @@ define(["require", "jquery", "constants", "underscore"], function(require, $, co
             if(_.has(Component, "preCompile"))
               Component.preCompile.apply(Component);
 
-            //TODO: if there is a problem with nested components :
-            //use jquery to check for automatic components in the "baseTemplate".
-            //if any, register them and remove auto-start-component attributes
-            //in order to prevent triggering the $apply inside the parent $apply.
-            //and then start the registered components (which are automatic).
-
-            //debugger;
-
             //we need the $apply function to persist the $compile work.
             $rootScope.$apply(function()
             {
               //load its view and replace the original container by the view.
               var angularElement = $compile(baseTemplate)($scope);
+              $element.empty();
               $element.append(angularElement);
 
               //it verfies whether the component has the loadComplete method to call it.
@@ -56,19 +49,19 @@ define(["require", "jquery", "constants", "underscore"], function(require, $, co
         if( _.has($attrs, "autoStartComponent") ) {
           //automatically start a component
           bootstrapComponent();
-        } else {
-          //manually start a component
-          $rootScope.$on("start-" + component, function() {
-            bootstrapComponent();
-          });
         }
 
         //removing a component is as simple as removing its html content,
         //we can't (& must not) unload the dependencies from RequireJS 
         //neither from angular.
         var removeComponent = function() {
-          $($element).html("");
+          $element.empty();
         };
+
+        //listening for the stop event to manually start a component.
+        $rootScope.$on("start-" + component, function() {
+          bootstrapComponent();
+        });
 
         //listening for the stop event, to remove the component.
         $rootScope.$on("stop-" + component, function() {
